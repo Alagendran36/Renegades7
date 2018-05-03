4ab33b6d372b2f0287f1c00b20e04d37c54175a3
Alagendran36 <38939810+Alagendran36@users.noreply.github.com>
2018-05-03 14:42:27 +0530

    Add files via upload

---
+++ app/src/main/java/com/delaroystudios/pizza/Checkout.java
@@ -0,0 +1,14 @@
+package com.delaroystudios.pizza;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+public class Checkout extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+        }
+}
+
